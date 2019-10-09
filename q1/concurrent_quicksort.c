3a124df167c5624570cb3a1153b4a1ff35dd0acb
pranav-kirsur <pranav.kirsur@gmail.com>
2019-10-10 00:35:50 +0530

    added thread

--- q1/concurrent_quicksort.c
+++ q1/concurrent_quicksort.c
@@ -142,7 +142,81 @@ void concurrent_quicksort(int arr[], int l, int r)
     return;
 }
 
+//struct for passing arguments to threaded quicksort
+struct arg
+{
+    int l;
+    int r;
+    int *arr;
+};
+
 //threaded quicksort
+void *threaded_quicksort(void *a)
+{
+    //typecast void pointer
+    struct arg *args = (struct arg *)a;
+
+    //get arguments
+    int l = args->l;
+    int r = args->r;
+    int *arr = args->arr;
+
+    if (r < l)
+    {
+        return NULL;
+    }
+
+    if (r - l <= 4)
+    {
+        //do insertion sort
+        int key;
+        for (int i = l + 1; i <= r; i++)
+        {
+            key = arr[i];
+            int j = i - 1;
+            while (j >= l && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        return NULL;
+    }
+
+    //recurse for quicksort
+    int pivot_index = partition(arr, l, r);
+
+    //sort left side in one thread
+    pthread_t tid1;
+
+    //args for the thread
+    struct arg args_left;
+    args_left.l = l;
+    args_left.r = pivot_index - 1;
+    args_left.arr = arr;
+
+    //create the thread
+    pthread_create(&tid1, NULL, threaded_quicksort, &args_left);
+
+    //sort right side in another thread
+    pthread_t tid2;
+
+    //args for the thread
+    struct arg args_right;
+    args_right.l = pivot_index + 1;
+    args_right.r = r;
+    args_right.arr = arr;
+
+    //create thread
+    pthread_create(&tid2, NULL, threaded_quicksort, &args_right);
+
+    //wait for the two threads to complete
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+
+    return NULL;
+}
 
 void runsorts(int n)
 {
@@ -157,11 +231,13 @@ void runsorts(int n)
         scanf("%d", &arr[i]);
     }
 
-    //make a copied array for comparision
+    //make two copied arrays for comparision
     int brr[n + 1];
+    int crr[n + 1];
     for (int i = 0; i < n; i++)
     {
         brr[i] = arr[i];
+        crr[i] = arr[i];
     }
 
     printf("Running concurrent_quicksort for n = %d\n", n);
@@ -177,6 +253,26 @@ void runsorts(int n)
     printf("time = %Lf\n", en - st);
     long double t1 = en - st;
 
+    //multithreaded quicksort args
+    pthread_t tid;
+    struct arg a;
+    a.l = 0;
+    a.r = n - 1;
+    a.arr = crr;
+
+    printf("Running multithreaded concurrent quicksort for n = %d\n", n);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+    st = ts.tv_nsec / (1e9) + ts.tv_sec;
+
+    //multithreaded quicksort
+    pthread_create(&tid, NULL, threaded_quicksort, &a);
+    pthread_join(tid, NULL);
+
+    clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+    en = ts.tv_nsec / (1e9) + ts.tv_sec;
+    printf("time = %Lf\n", en - st);
+    long double t2 = en - st;
+
     printf("Running normal quicksort for n = %d\n", n);
     clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
     st = ts.tv_nsec / (1e9) + ts.tv_sec;
@@ -189,7 +285,7 @@ void runsorts(int n)
     printf("time = %Lf\n", en - st);
     long double t3 = en - st;
 
-    printf("Concurrent quicksort ran %Lf times faster than normal quicksort\n", t3 / t1);
+    printf("Normal quicksort ran %Lf times faster than multiprocess concurrent quicksort and %Lf times faster than multithreaded concurrent quicksort\n", t1 / t3, t1 / t2);
 
     //release shared memory
     shmdt(arr);
