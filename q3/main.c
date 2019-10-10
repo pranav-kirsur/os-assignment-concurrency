c4bc595c106832aff91c324c3405d9ef89f58067
pranav-kirsur <pranav.kirsur@gmail.com>
2019-10-11 05:20:24 +0530

    added rider threads

--- q3/main.c
+++ q3/main.c
@@ -8,6 +8,13 @@
 #define STATE_ON_RIDE_POOL_FULL 2
 #define STATE_ON_RIDE_POOL_ONE 3
 
+//Cab types
+#define TYPE_PREMIER 0
+#define TYPE_POOL 1
+
+//Max waiting time for rider
+#define MAX_WAIT_TIME 5
+
 struct Cab
 {
     int id;
@@ -19,10 +26,35 @@ struct Rider
     int id;
 };
 
+//global variable that contains array of cab structs
 struct Cab *cabs;
+
+//returns random number in the given range(both inclusive)
+int random(int lower_bound, int upper_bound)
+{
+    return (rand() % upper_bound - lower_bound + 1) + lower_bound;
+}
+
+void book_cab(int cab_type, int maxwaitime, int ride_time)
+{
+
+}
 
 void *rider(void *args)
 {
+    //get arguments for rider
+    struct Rider *rider_args = (struct Rider *)args;
+    int id = rider_args->id;
+
+    //generate random arguments for booking cab
+    int cab_type = random(0, 1);
+    int ride_time = random(1, 5);
+
+    //book the cab
+    book_cab(cab_type, MAX_WAIT_TIME, ride_time);
+
+    //end the thread
+    exit(1);
 }
 
 int main()
@@ -50,6 +82,12 @@ int main()
         struct Rider args;
         args.id = i;
         pthread_create(&rider_threads[i], NULL, rider, &args)
+    }
+
+    //wait for threads to complete
+    for (int i = 0; i < m; i++)
+    {
+        pthread_join(rider_threads[i], NULL);
     }
     return 0;
 }
