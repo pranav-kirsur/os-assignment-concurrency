a01b1d732bcb168c178daab293c52b5a3ce5be67
pranav-kirsur <pranav.kirsur@gmail.com>
2019-10-12 15:45:50 +0530

    started q2

---
+++ q2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <pthread.h>
+
+//chefs,serving tables, student
+int m,n,k;
+
+int main()
+{
+    printf("Input the number of chefs, the number of serving tables, the number of students\n");
+    scanf("%d%d%d",&m,&n,&k);
+    
+    return 0;
+}
